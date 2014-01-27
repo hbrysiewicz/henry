2bd907637c1f4bb6b4d93f68ec82399c29880029
Heather Brysiewicz <heather.brysiewicz@thecontrolgroup.com>
2014-01-27 15:13:14 -0800

    Updates to side nav and routes

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,9 +1,15 @@
 App.Controller = Ember.Controller.extend({
   needs: ['login'],
   token: Ember.computed.alias('controllers.login.token'),
-  user: {
-    name: "User",
-    admin: false,
-    icon: 'public/img/avatar.png'
-  }
+  user: Ember.Object.extend({
+    id: 1,
+    firstName: "Heather",
+    lastName: "Brysiewicz",
+    email: "heather.brysiewicz@thecontrolgroup.com",
+    admin: true,
+    icon: '/assets/img/avatar.png',
+    name: function() {
+      return this.get('firstName') + ' ' + this.get('lastName');
+    }.property('firstName','lastName')
+  }).create()
 });
--- app/router.js
+++ app/router.js
@@ -3,8 +3,34 @@ App.Router.reopen({
 });
 
 App.Router.map(function() {
+
   this.route('login');
   this.route('logout');
   this.route('signup');
+
+  this.resource('user', function() {
+    this.route('settings', {path: '/settings/:id'});
+  });
+
+  this.resource('bi', function() {
+    this.route('company');
+  });
+
+  // Admin Routes
+  this.resource('admin', function() {
+    this.resource("users", function(){
+      this.route("create", { path: '/create' });
+      this.route("edit", { path: '/edit/:id' });
+    });
+    this.resource("roles", function(){
+      this.route("create");
+      this.route("edit", { path: '/edit/:id' });
+    });
+    this.resource("resources", function(){
+      this.route("create");
+      this.route("edit", { path: '/edit/:id' });
+      this.route("delete", { path: '/delete/:id' });
+    });
+  })
 });
 
